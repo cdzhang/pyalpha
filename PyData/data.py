cc5ab8f78f7313da6f55050773d5ac08572786a7
Chenxi He <chenxihe@Chenxis-MacBook-Pro.local>
2020-07-13 23:51:28 +0800

    add BaseData

--- PyData/data.py
+++ PyData/data.py
@@ -1,1 +1,16 @@
-class
+import tushare as ts
+from PyUtils.common import lazy_property
+
+
+class BaseData:
+    def __init__(self, code, start, end, date_type='D'):
+        self.code = code
+        self.start = start
+        self.end = end
+        self.date_type = date_type
+
+    @lazy_property
+    def get_hist_data(self):
+        return ts.get_hist_data(self.code, self.start, self.end, self.date_type)
+
+
