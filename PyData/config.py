7ef38c4271d06c6bbd160f1401490a39ace92f47
Chenxi He <chenxihe@Chenxis-MacBook-Pro.local>
2020-07-17 02:43:38 +0800

    #1 update base stock class

--- PyData/config.py
+++ PyData/config.py
@@ -4,3 +4,4 @@ from datetime import datetime
 HIST_DIR = os.path.join(os.path.dirname(__file__), 'hist_data')
 HIST_START = datetime(year=2018, day=1, month=1)
 HIST_END = datetime(year=2020, day=13, month=7)
+TS_TOKEN = os.getenv('TS_TOKEN')
--- PyData/data.py
+++ PyData/data.py
@@ -1,14 +1,39 @@
+from abc import ABCMeta, abstractmethod
 import tushare as ts
 from PyUtils.common import lazy_property
 from glob import glob
 import os
-from PyData.config import HIST_DIR, HIST_END, HIST_START
+from PyData.config import HIST_DIR, HIST_END, HIST_START, TS_TOKEN
 from PyData.utils import *
 
 hist_file_ls = glob(os.path.join(HIST_DIR, '*.csv'))
+ts.set_token(TS_TOKEN)
+ts_pro = ts.pro_api()
 
 
-class SingleSecurity:
+class TuShareTool:
+    @staticmethod
+    def get_hist_ts(code, start, end, date_type):
+        if isinstance(start, str):
+            start = pd.to_datetime(start)
+
+        if isinstance(end, str):
+            end = pd.to_datetime(end)
+
+        if start >= HIST_START and end <= HIST_END and glob(os.path.join(HIST_DIR, f'{code}*.csv')):
+            file_dir = glob(os.path.join(HIST_DIR, f'{code}*.csv'))[0]
+            df_hist = read_hist_csv(file_dir)
+            df_hist = df_hist[(df_hist.date <= end) & (df_hist.date >= start)]
+        else:
+            df_hist = ts.get_hist_data(code, str(start), str(end), date_type).reset_index()
+        return df_hist.sort_values('date', ascending=True)
+
+    @staticmethod
+    def get_tick_data(code, date):
+        return ts.get_tick_data(code, date, src='tt')
+
+
+class Stock(TuShareTool):
     def __init__(self, code, start, end, date_type='D'):
         self.code = code
         self.start = start
@@ -16,16 +41,17 @@ class SingleSecurity:
         self.date_type = date_type
 
     @lazy_property
-    def get_hist_data(self):
-        if pd.to_datetime(self.start) >= HIST_START and pd.to_datetime(self.end) <= HIST_END and glob(
-                os.path.join(HIST_DIR, f'{self.code}*.csv')):
-            file_dir = glob(os.path.join(HIST_DIR, f'{self.code}*.csv'))[0]
-            df_hist = read_hist_csv(file_dir)
-        else:
-            df_hist = ts.get_hist_data(self.code, self.start, self.end, self.date_type)
-        return df_hist
+    def df_get_hist(self):
+        return self.get_hist_ts(self.code, self.start, self.end, self.date_type)
+
+    # @lazy_property
+    # def get_industry(self):
+    #     # todo add code2industry json file
 
     @lazy_property
-    def get_industry(self):
-        return ts.get_industry_classified('sw')
-
+    def df_get_close_return(self):
+        """return list of daily return close price"""
+        df = self.df_get_hist[['date', 'p_change', 'close', 'price_change']]
+        p0 = df.close.iloc[0]
+        df['cum_return'] = [(i - p0) / p0 for i in df.close]
+        return df
